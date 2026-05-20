bd3574a5c970159b51161429f50fd7d8ed811b04
deChilonio <168724847+oneandonlybasti@users.noreply.github.com>
2026-05-20 21:09:47 +0200

    intro clip and working main menu
    - added intro clip
    - starting the game from main menu now works

---
+++ scenes/ui_scenes/main_menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main/intro_clip.tscn")
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
