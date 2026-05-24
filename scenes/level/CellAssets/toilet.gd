c830d0a4f1c30f3340c6953d861456e976610347
Florian W <pulv3rtoastmann@gmail.com>
2026-05-24 19:04:05 +0200

    button spam kind of done

--- scenes/level/CellAssets/toilet.gd
+++ scenes/level/CellAssets/toilet.gd
@@ -2,35 +2,22 @@ extends StaticBody3D
 
 @onready var ToiletUi = preload("res://scenes/main/minigames/ToiletMiniGame.tscn")
 @onready var mainScene = get_tree().get_root().get_node("Main_Scene/Player/UILayer")
-var uiInstance
-var button
-var disgustBar
 
-signal ToiletMiniGameStarted();
-signal ToiletMiniGameEnded();
+signal ToiletMiniGameStarted()
+signal ToiletMiniGameEnded()
+
+var interaced_with = false
 
 func interact():
 	startMinigame()
 
 func startMinigame():
 	ToiletMiniGameStarted.emit()
-	uiInstance = ToiletUi.instantiate()
-	button = uiInstance.get_child(0).get_node("Click Logic")
-	disgustBar = uiInstance.get_child(1).get_node("ProgressBar")
-	button.pressed.connect(_on_button_pressed)
+	var uiInstance = ToiletUi.instantiate()
 	mainScene.add_child(uiInstance)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	uiInstance.ToiletMiniGameUiDeleted.connect(_on_toilet_minigame_ui_delete)
 
-func _on_button_pressed():
-	disgustBar.value += disgustBar.step
-	if disgustBar.value > 100.0:
-		endMiniGame()
-
-func endMiniGame(): # cleanup
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	uiInstance.queue_free()
+func _on_toilet_minigame_ui_delete():
 	ToiletMiniGameEnded.emit()
-
-func _process(delta: float) -> void:
-	if is_instance_valid(disgustBar) and disgustBar.value > 0.0:
-		disgustBar.value -= 3 * delta
+	interaced_with = true
--- scenes/main/minigames/toilet_mini_game.gd
+++ scenes/main/minigames/toilet_mini_game.gd
@@ -3,5 +3,27 @@ extends Control
 @onready var button = $"Menu Button/Click Logic"
 @onready var disgustBar = $"ProgressBarContainer/ProgressBar"
 
-func _on_toilet_mini_game_started():
-	print("test")
+@export var decline_rate = 15
+var current_bar_value = 1
+
+signal ToiletMiniGameUiDeleted()
+
+func _ready():
+    button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+    current_bar_value += disgustBar.step
+    disgustBar.value = current_bar_value
+    if disgustBar.value >= 100.0:
+        endMiniGame()
+
+func _physics_process(delta: float) -> void:
+    current_bar_value -= decline_rate * delta
+    current_bar_value = max(current_bar_value, 0)
+    print(current_bar_value)
+    disgustBar.value = current_bar_value
+
+func endMiniGame():
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    ToiletMiniGameUiDeleted.emit()
+    queue_free()
