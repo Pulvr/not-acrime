950c253b397d9a4fae5867dcc19b938299bd071f
Florian W <pulv3rtoastmann@gmail.com>
2026-05-27 08:50:55 +0200

    fixed dialog starting again on press e

--- resources/test_assets/talk_Pill/dialog_interactable.gd
+++ resources/test_assets/talk_Pill/dialog_interactable.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 @export var timeline: DialogicTimeline
 
 func startDialog():
-	if timeline != null:
+	if timeline != null and Dialogic.current_timeline == null:
 		Dialogic.start(timeline)
 	else:
 		print("No timeline assigned to this NPC!")
