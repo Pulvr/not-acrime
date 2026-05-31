4dfbd591e470a6b97db1adf823283b71846f8172
Florian W <pulv3rtoastmann@gmail.com>
2026-05-31 11:42:46 +0200

    updated spam minigame to lmb and rmb icons need to change

--- scenes/ui_scenes/minigames/toilet_mini_game.gd
+++ scenes/ui_scenes/minigames/toilet_mini_game.gd
@@ -2,28 +2,44 @@ extends Control
 
 @onready var button = $"Menu Button/Click Logic"
 @onready var disgustBar = $"ProgressBarContainer/ProgressBar"
+@onready var lmb_icon = $"LeftMouse"
+@onready var rmb_icon = $"RightMouse"
 
 @export var decline_rate = 15
-@export var click_reward = 5
+@export var click_reward = 3
 var current_bar_value = 0
+
+var lmb_last_pressed = false
+var rmb_last_pressed = true
 
 signal ToiletMiniGameUiDeleted()
 
-func _ready():
-    button.pressed.connect(_on_button_pressed)
-
-func _on_button_pressed():
-    current_bar_value += click_reward
-    disgustBar.value = current_bar_value
-    if disgustBar.value >= 100.0:
-        endMiniGame()
-
 func _physics_process(delta: float) -> void:
-    current_bar_value -= decline_rate * delta
-    current_bar_value = max(current_bar_value, 0)
-    disgustBar.value = current_bar_value
+	current_bar_value -= decline_rate * delta
+	current_bar_value = max(current_bar_value, 0)
+	disgustBar.value = current_bar_value
 
 func endMiniGame():
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-    ToiletMiniGameUiDeleted.emit()
-    queue_free()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	ToiletMiniGameUiDeleted.emit()
+	queue_free()
+
+
+func _on_click_logic_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and rmb_last_pressed and event.pressed:
+			current_bar_value += click_reward
+			disgustBar.value = current_bar_value
+			lmb_last_pressed = true
+			rmb_last_pressed = false
+			lmb_icon.visible = false
+			rmb_icon.visible = true
+		if event.button_index == 2 and lmb_last_pressed and event.pressed: 
+			current_bar_value += click_reward
+			disgustBar.value = current_bar_value
+			rmb_last_pressed = true
+			lmb_last_pressed = false
+			lmb_icon.visible = true
+			rmb_icon.visible = false
+	if disgustBar.value >= 100.0:
+		endMiniGame()
