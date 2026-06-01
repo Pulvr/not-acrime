afd5e52e5f66ccde7ff60646f8c16b7f762c4d8c
Oskar <anskarrcoding@tuta.com>
2026-06-01 12:05:24 +0200

    added settings_menu scene

--- scenes/ui_scenes/main_menu.gd
+++ scenes/ui_scenes/main_menu.gd
@@ -3,5 +3,8 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main/intro_clip.tscn")
 
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui_scenes/settings_menu.tscn")
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
