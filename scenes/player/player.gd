f50fc1d2065e876ce6497d79349f42f2ebc956e4
Florian W <pulv3rtoastmann@gmail.com>
2026-06-01 07:19:48 +0200

    diabled jump

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 @export var SPEED = 5
-@export var JUMP_VELOCITY = 4.5
 
 @export var mouse_sensitivity = 0.002
 @export var can_move = true
@@ -104,9 +103,6 @@ func walk_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor() and can_move:
-		velocity.y = JUMP_VELOCITY
-	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction and can_move:
