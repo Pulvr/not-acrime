83beab8acf4ecac21ea51ccb23a5a9b0468900e9
deChilonio <168724847+oneandonlybasti@users.noreply.github.com>
2026-06-03 10:53:54 +0200

    play sounds in toilet minigame

--- scenes/ui_scenes/minigames/toilet_mini_game.gd
+++ scenes/ui_scenes/minigames/toilet_mini_game.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var disgustBar = $"ProgressBarContainer/ProgressBar"
 @onready var lmb_icon = $"LeftMouse"
 @onready var rmb_icon = $"RightMouse"
+@onready var splash_player = $SplashSoundPlayer
+@onready var success_player = $SuccessSoundPlayer
 
 @export var decline_rate = 15
 @export var click_reward = 3
@@ -11,6 +13,10 @@ var current_bar_value = 0
 
 var lmb_last_pressed = false
 var rmb_last_pressed = true
+
+var click_count = 0
+var game_ended = false
+var splash_sound_intervall = 10 # Clicks we need to play a splash sound
 
 signal ToiletMiniGameUiDeleted()
 
@@ -20,13 +26,19 @@ func _physics_process(delta: float) -> void:
 	disgustBar.value = current_bar_value
 
 func endMiniGame():
+	visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	success_player.play()
+	await success_player.finished
 	ToiletMiniGameUiDeleted.emit()
 	queue_free()
 
 
 func _on_click_logic_gui_input(event: InputEvent) -> void:
+	if game_ended:
+		return
 	if event is InputEventMouseButton:
+		var valid_click = false
 		if event.button_index == 1 and rmb_last_pressed and event.pressed:
 			current_bar_value += click_reward
 			disgustBar.value = current_bar_value
@@ -34,6 +46,7 @@ func _on_click_logic_gui_input(event: InputEvent) -> void:
 			rmb_last_pressed = false
 			lmb_icon.visible = false
 			rmb_icon.visible = true
+			valid_click = true
 		if event.button_index == 2 and lmb_last_pressed and event.pressed: 
 			current_bar_value += click_reward
 			disgustBar.value = current_bar_value
@@ -41,5 +54,12 @@ func _on_click_logic_gui_input(event: InputEvent) -> void:
 			lmb_last_pressed = false
 			lmb_icon.visible = true
 			rmb_icon.visible = false
-	if disgustBar.value >= 100.0:
+			valid_click = true
+		if valid_click:
+			click_count += 1
+			if click_count % splash_sound_intervall == 0:
+				splash_player.pitch_scale = randf_range(0.9, 1.1)
+				splash_player.play()
+	if disgustBar.value >= 100.0 and not game_ended:
+		game_ended = true
 		endMiniGame()
