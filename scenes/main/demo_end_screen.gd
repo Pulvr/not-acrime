2c9cb165774840f60db7b09a904788fd466cd9a0
Oskar <anskarrcoding@tuta.com>
2026-07-02 10:01:04 +0200

    sample text added that still needs refinement

--- scenes/main/demo_end_screen.gd
+++ scenes/main/demo_end_screen.gd
@@ -9,14 +9,32 @@ var text1_playtime = 3.5
 var text2_playtime = 1.5
 var text3_playtime = 4.0
 var text4_playtime = 2.2
+var text5_playtime = 3
+var text6_playtime = 3
+var text7_playtime = 3
+var text8_playtime = 3
+var text9_playtime = 3
+var text10_playtime = 3
+var text11_playtime = 3
+var text12_playtime = 3
 var last_visible_chars = 0
 var fade_out_time = 2.5
 var idle_time = 2.0
 
-@export var text1 = "This is the end of our playtest demo."
-@export var text2 = "Thanks for playing."
-@export var text3 = "A game by Florian Wendel, Oskar Kotte and Sebastian Grewe."
-@export var text4 = "Developed using Godot 4 and Dialogic 2."
+@export var text1 = "There are still countries imposing the death penalty upon homosexuality.."
+@export var text2 = "Did you read the newspaper headlines on the wall?"
+@export var text3 = "Those were all real."
+@export var text4 = "Criminilization of LGBTIQ is rising still."
+@export var text5 = "Stand together."
+@export var text6 = "Personality Rights are Human Rights."
+@export var text7 = "..."
+@export var text8 = "Thank you for playing!"
+@export var text9 = "This is the end of our playtest demo..."
+@export var text10 = "...made by Florian Wendel, Sebastian Grewe and Oskar Kotte."
+@export var text11 = "Proudly developed using Godot 4 and Dialogic 2."
+@export var text12 = "Sources: 	source1 \n
+							    source2 \n
+							    source3"
 
 func _ready():
 	text_label.text = text1
@@ -35,6 +53,30 @@ func _ready():
 	tween.tween_interval(idle_time)
 	tween.tween_callback(change_to_text4)
 	tween.tween_property(text_label, "visible_ratio", 1.0, text4_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text5)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text5_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text6)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text6_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text7)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text7_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text8)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text8_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text9)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text9_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text10)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text10_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text11)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text11_playtime)
+	tween.tween_interval(idle_time)
+	tween.tween_callback(change_to_text12)
+	tween.tween_property(text_label, "visible_ratio", 1.0, text12_playtime)
 	tween.tween_interval(idle_time)
 	tween.tween_property(fade_overlay, "modulate:a", 1.0, fade_out_time)
 	tween.tween_interval(idle_time)
@@ -71,6 +113,46 @@ func change_to_text4():
 	text_label.visible_ratio = 0.0
 	last_visible_chars = 0
 
+func change_to_text5():
+	text_label.text = text5
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+	
+func change_to_text6():
+	text_label.text = text6
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+
+func change_to_text7():
+	text_label.text = text7
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+
+func change_to_text8():
+	text_label.text = text8
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+	
+func change_to_text9():
+	text_label.text = text9
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+	
+func change_to_text10():
+	text_label.text = text10
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+	
+func change_to_text11():
+	text_label.text = text11
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+	
+func change_to_text12():
+	text_label.text = text12
+	text_label.visible_ratio = 0.0
+	last_visible_chars = 0
+	
 func switch_to_main_menu():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().change_scene_to_file(main_menu_path)
